4cbf836617b6ad109066666f759da5711feeee31
skatsuta <katsuta_soshi@cyberagent.co.jp>
2014-09-05 11:12:36 +0900

    Add factorial() and formatFactorial()

--- src/main/scala/MyModule.scala
+++ src/main/scala/MyModule.scala
@@ -6,12 +6,29 @@ object MyModule {
   def abs(n: Int): Int =
     if (n < 0) -n
     else n
+
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int = n match {
+      case n if n <= 0 => acc
+      case _ => go(n - 1, n * acc)
+    }
+    go(n, 1)
+  }
+
   private def formatAbs(x: Int) = {
     val msg = "The absolute value of %d is %d"
     msg.format(x, abs(x))
   }
+
+  private def formatFactorial(n: Int) = {
+    val msg = "The factorial of %d is %d."
+    msg format (n, factorial(n))
+  }
+
   def main(args: Array[String]): Unit =
     println(formatAbs(-42))
+    println(formatFactorial(7))
 
   def fib_normal(n: Int): Int =
     n match {
