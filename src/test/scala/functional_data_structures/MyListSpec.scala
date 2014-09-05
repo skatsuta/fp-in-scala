4e012282f324df59f657a193ac53a02bc183a89d
skatsuta <katsuta_soshi@cyberagent.co.jp>
2014-09-06 02:47:25 +0900

    Refactor tests by using ScalaCheck

--- src/test/scala/functional_data_structures/MyListSpec.scala
+++ src/test/scala/functional_data_structures/MyListSpec.scala
@@ -1,36 +1,33 @@
 package functional_data_structures
 
+import org.specs2.ScalaCheck
+import org.scalacheck.Prop.forAll
 import org.specs2.mutable.Specification
 
-class MyListSpec extends Specification {
+class MyListSpec extends Specification with ScalaCheck {
   "tail" should {
-    "return a list except the first element" in {
-      MyList.tail(List(1, 2, 3)) must_== List(2, 3)
-    }
-
     "return Nil when applied to Nil" in {
       MyList.tail(Nil) must_== Nil
+    }
+
+    "return a list except the first element" ! forAll {
+      xs: List[Int] => xs.nonEmpty ==> (MyList.tail(xs) == xs.tail)
     }
   }
 
   "setHead" should {
-    "return lists that have different values in its head when applied to different values" in {
-      MyList.setHead(Nil, 1) must_== Nil
-      MyList.setHead(List(1), 2) must_== List(2)
-      MyList.setHead(List("c", "b"), "a") must_== List("a", "b")
+    "return Nil when applied to Nil" ! forAll {
+      n: Int => MyList.drop(Nil, n) must_== Nil
+    }
+
+    "replace the first element" ! forAll {
+      (xs: List[Int], x: Int) => xs.nonEmpty ==> (MyList.setHead(xs, x) == x :: xs.tail)
     }
   }
 
   "drop" should {
-    "remove the first n elements from a list" in {
-      MyList.drop(List(1), -1) must_== List(1)
-      MyList.drop(List(1), 0) must_== List(1)
-      MyList.drop(Nil, 3) must_== Nil
-
-      MyList.drop(List(1), 1) must_== Nil
-      MyList.drop(List(1), 2) must_== Nil
-      MyList.drop(List(1, 2), 1) must_== List(2)
-      MyList.drop(List(1, 2, 3), 2) must_== List(3)
+    "remove the first n elements from a list" ! prop {
+      (xs: List[Int], n: Int) => MyList.drop(xs, n) must_== xs.drop(n)
     }
   }
 }
