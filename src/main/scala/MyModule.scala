abe4c48577d11ba7c1ffbd2ce97f9091aed5d576
skatsuta <katsuta_soshi@cyberagent.co.jp>
2014-09-05 11:02:17 +0900

    Solve Exercise 2.1

--- src/main/scala/MyModule.scala
+++ src/main/scala/MyModule.scala
@@ -12,4 +12,20 @@ object MyModule {
   }
   def main(args: Array[String]): Unit =
     println(formatAbs(-42))
+
+  def fib_normal(n: Int): Int =
+    n match {
+      case 0 | 1 => n
+      case _ => fib_normal(n - 1) + fib_normal(n - 2)
+    }
+
+  def fib_tailrec(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, prev: Int, next: Int): Int = n match {
+      case 0 => prev
+      case 1 => next
+      case _ => go(n - 1, next, prev + next)
+    }
+    go(n, 0, 1)
+  }
 }
